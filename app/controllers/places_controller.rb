8486e9a9d01851569a54e4757aba50c9ecab9e8b
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-09-18 10:36:00 +0000

    Updated places controller to save form input to db

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,20 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.order(:created_at).page(params[:page]).per(10)
+    @places = Place.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def new
     @places = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end
 end
