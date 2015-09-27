11432f62c57b2e36674dd3ad44ce7ff595de9ef5
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-09-27 10:10:46 +0000

    Update comments controller and show page to edit/delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,41 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :only => [:create, :edit, :update, :destroy]
   def create
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(:user => current_user))
     redirect_to place_path(@place)
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+
+    if @place.comments.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    if @place.comments.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
+
+    @place.update_attributes(comment_params)
+    if @place.comments.valid?
+      redirect_to root_path
+    else
+      render :edit, :status => :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    if @place.comments.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
+
+    @place.comments.destroy
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
-    resources :comments, :only => :create
+    resources :comments #, :only => :create
   end
 
   resources :places do
