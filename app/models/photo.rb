cbf7059afdb12bf199bbedb2254c0a606e583ad7
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-10-18 09:35:40 +0000

    Add reprocess method to photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,16 @@
 class Photo < ActiveRecord::Base
   belongs_to :place
   mount_uploader :picture, PictureUploader 
+
+   def reprocess
+    begin
+      self.process_photo_upload = true
+      self.picture.cache_stored_file!
+      self.picture.retrieve_from_cache!(picture.cache_name)
+      self.picture.recreate_versions!
+      self.save!
+    rescue => e
+      STDERR.puts  "ERROR: Photo: #{id} -> #{e.to_s}"
+    end
+  end
 end
